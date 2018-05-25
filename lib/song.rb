b68e492062fe31f8642aaa4a47dc6d4265963b85
Amar Chatman <amarjerome@gmail.com>
2018-05-25 19:39:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,16 +13,14 @@ class Song
     @@songs << self
   end
 
-
-
   def artist=(artist)
     @artist = artist
   end
 
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 
   def self.all
     @@songs
